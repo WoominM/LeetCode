92bd87e2737b7ef1def4f6e83016c440c6e7342c
Woomin <65762488+WoominM@users.noreply.github.com>
2021-10-27 11:12:22 +0800

    Time: 140 ms (59.22%), Space: 16.6 MB (86.00%) - LeetHub

--- single-number/single-number.py
+++ single-number/single-number.py
@@ -1,8 +1,17 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
+        # for i in range(len(nums)):
+        #     num = nums[0]
+        #     nums.pop(nums.index(num))
+        #     if num not in nums:
+        #         return num
+        #     else:
+        #         nums.pop(nums.index(num))
+        
+        dic = {}
         for i,num in enumerate(nums):
-            nums.pop(nums.index(num))
-            if num not in nums:
-                return num
+            if num in dic.keys():
+                del dic[num]
             else:
-                nums.insert(i, num)
+                dic[num] = 1
+        return list(dic)[0]
