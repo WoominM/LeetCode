0883103e01281da46dd7fcef56cf4f87d2e3805c
Woom <65762488+WoominM@users.noreply.github.com>
2022-05-26 14:58:25 +0800

    Time: 41 ms (52.52%), Space: 13.9 MB (12.91%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.py
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.py
@@ -8,16 +8,35 @@ class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         if root == None:
             return
-        self.it = []        
-        return self.dfs(root, self.it)
-    
-    def dfs(self, root, it):
-        if root.left == None:
-            it.append(root.val)
-        else:
-            self.dfs(root.left, it)
-            it.append(root.val)
-        if root.right == None:
-            return it
-        self.dfs(root.right, it)
+        stack = []
+        it = []
+        mroot = root
+        while stack or mroot is not None:
+            while mroot is not None:
+                stack.append(mroot)
+                mroot = mroot.left
+            mroot = stack.pop()
+            it.append(mroot.val)
+            mroot = mroot.right
         return it
+                
+    
+        
+### recursive approach
+
+#     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+#         if root == None:
+#             return
+#         self.it = []        
+#         return self.dfs(root)
+    
+#     def dfs(self, root):
+#         if root.left == None:
+#             self.it.append(root.val)
+#         else:
+#             self.dfs(root.left)
+#             self.it.append(root.val)
+#         if root.right == None:
+#             return self.it
+#         self.dfs(root.right)
+#         return self.it
