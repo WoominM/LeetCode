e98985fe23144ad5d47074e25f961888f72890b5
Woom <65762488+WoominM@users.noreply.github.com>
2022-05-27 23:41:01 +0800

    Time: 35 ms (74.60%), Space: 14 MB (29.58%) - LeetHub

--- 100-same-tree/100-same-tree.py
+++ 100-same-tree/100-same-tree.py
@@ -4,26 +4,53 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import deque
 class Solution:
 ### iterateively
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if (not p and q) or (p and not q):
-            return False
-        stack1, stack2 = [], []
-        while (stack1 and stack2) or (p and q):
-            while p and q:  
-                if p.val != q.val:
-                    return False
-                stack1.append(p)
-                stack2.append(q)
-                p, q = p.left, q.left
-            if (not p and q) or (p and not q):
+        def check(p, q):
+            # if both are None
+            if not p and not q:
+                return True
+            # one of p and q is None
+            if not q or not p:
                 return False
-            p, q = stack1.pop(), stack2.pop()
-            p, q = p.right, q.right
-        if (not p and q) or (p and not q):
-            return False    
+            if p.val != q.val:
+                return False
+            return True
+        
+        deq = deque([(p, q),])
+        while deq:
+            p, q = deq.popleft()
+            if not check(p, q):
+                return False
+            if p:
+                deq.append((p.left, q.left))
+                deq.append((p.right, q.right))        
         return True
+        
+        
+        
+        
+### iterateively2
+    # def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+    #     if (not p and q) or (p and not q):
+    #         return False
+    #     stack1, stack2 = [], []
+    #     while (stack1 and stack2) or (p and q):
+    #         while p and q:  
+    #             if p.val != q.val:
+    #                 return False
+    #             stack1.append(p)
+    #             stack2.append(q)
+    #             p, q = p.left, q.left
+    #         if (not p and q) or (p and not q):
+    #             return False
+    #         p, q = stack1.pop(), stack2.pop()
+    #         p, q = p.right, q.right
+    #     if (not p and q) or (p and not q):
+    #         return False    
+    #     return True
      
         
         
