3ad9fba14c85971accbf6a417886f6fdee6bb298
Woom <65762488+WoominM@users.noreply.github.com>
2022-06-01 16:27:33 +0800

    Time: 55 ms (31.94%), Space: 14 MB (21.73%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.py
+++ 101-symmetric-tree/101-symmetric-tree.py
@@ -4,15 +4,40 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import deque
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        return self.helper(root, root)
-        
-    def helper(self, p, q):
+        p, q = root, root
+        que = deque([(p, q)])
+        while que:
+            p, q = que.popleft()
+            if not self.check(p, q):
+                return False
+            if p:
+                que.append((p.left, q.right))
+                que.append((p.right, q.left))
+        return True
+            
+    def check(self, p, q):
         if not p and not q:
             return True
         if not p or not q:
             return False
         if p.val != q.val:
-            return False        
-        return self.helper(p.left, q.right) and self.helper(p.right, q.left)
+            return False   
+        return True
+
+        
+        
+### recursively
+#     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+#         return self.helper(root, root)
+        
+#     def helper(self, p, q):
+#         if not p and not q:
+#             return True
+#         if not p or not q:
+#             return False
+#         if p.val != q.val:
+#             return False        
+#         return self.helper(p.left, q.right) and self.helper(p.right, q.left)
