c9f4a7204986372b2533557c1e9ec9fe06aa796f
Woom <65762488+WoominM@users.noreply.github.com>
2022-06-02 04:10:14 +0800

    Time: 1155 ms (5.01%), Space: 55 MB (14.26%) - LeetHub

--- 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.py
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.py
@@ -9,6 +9,6 @@ class Solution:
         if not root:
             return 0
         if not root.left or not root.right:
-            return 1+ max(self.minDepth(root.left), self.minDepth(root.right))
-        return 1+ min(self.minDepth(root.left), self.minDepth(root.right))
+            return 1 + max(self.minDepth(root.left), self.minDepth(root.right))
+        return 1 + min(self.minDepth(root.left), self.minDepth(root.right))
     
