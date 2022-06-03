b8743b3d837a579deb89fa97c31a2264db2b3b2e
Woom <65762488+WoominM@users.noreply.github.com>
2022-06-04 07:24:50 +0800

    Time: 41 ms (93.95%), Space: 15.3 MB (15.54%) - LeetHub

---
+++ 112-path-sum/112-path-sum.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if not root:
+            return False
+        self.result = []
+        self.isleaf = []
+        self.sumvalue = 0
+        self.helper(root)
+        self.result = [self.result[i] if self.isleaf[i]!=-1 else -1001 for i in range(len(self.result))]
+        return targetSum in self.result
+            
+    def helper(self, root):
+        if root:
+            self.sumvalue += root.val
+            self.result.append(self.sumvalue)
+            self.isleaf.append(1 if not root.left and not root.right else -1)
+            self.helper(root.left)
+            self.helper(root.right)
+            self.sumvalue -= root.val
+            
+            
