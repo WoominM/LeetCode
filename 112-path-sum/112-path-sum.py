9c7a29b7e13b6e75f0903ba42786e18c7ef5179b
Woom <65762488+WoominM@users.noreply.github.com>
2022-06-04 07:28:50 +0800

    Time: 105 ms (5.33%), Space: 15 MB (92.71%) - LeetHub

--- 112-path-sum/112-path-sum.py
+++ 112-path-sum/112-path-sum.py
@@ -8,20 +8,29 @@ class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
         if not root:
             return False
-        self.result = []
-        self.isleaf = []
-        self.sumvalue = 0
-        self.helper(root)
-        self.result = [self.result[i] if self.isleaf[i]!=-1 else -1001 for i in range(len(self.result))]
-        return targetSum in self.result
+        if not root.left and not root.right and root.val == targetSum:
+            return True
+        
+        targetSum -= root.val
+        return self.hasPathSum(root.left, targetSum) or self.hasPathSum(root.right, targetSum)
+    
+#     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+#         if not root:
+#             return False
+#         self.result = []
+#         self.isleaf = []
+#         self.sumvalue = 0
+#         self.helper(root)
+#         self.result = [self.result[i] if self.isleaf[i]!=-1 else -1001 for i in range(len(self.result))]
+#         return targetSum in self.result
             
-    def helper(self, root):
-        if root:
-            self.sumvalue += root.val
-            self.result.append(self.sumvalue)
-            self.isleaf.append(1 if not root.left and not root.right else -1)
-            self.helper(root.left)
-            self.helper(root.right)
-            self.sumvalue -= root.val
+#     def helper(self, root):
+#         if root:
+#             self.sumvalue += root.val
+#             self.result.append(self.sumvalue)
+#             self.isleaf.append(1 if not root.left and not root.right else -1)
+#             self.helper(root.left)
+#             self.helper(root.right)
+#             self.sumvalue -= root.val
             
             
