64f6504a2373ad9fd4a570e61a89a9e0fb673108
Woom <65762488+WoominM@users.noreply.github.com>
2022-06-04 08:27:04 +0800

    Time: 41 ms (57.62%), Space: 13.8 MB (55.84%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.py
+++ 226-invert-binary-tree/226-invert-binary-tree.py
@@ -9,3 +9,11 @@ class Solution:
         if root:
             root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
             return root
+        
+        # stack = [root]
+        # while stack:
+        #     node = stack.pop()
+        #     if node:
+        #         node.left, node.right = node.right, node.left
+        #         stack += node.left, node.right
+        # return root
