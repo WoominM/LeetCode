2d02d82d65a82c0c3d76a482a4b0ba40f2bca07a
Woom <65762488+WoominM@users.noreply.github.com>
2022-07-14 19:07:55 +0900

    Time: 62 ms (73.07%), Space: 13.9 MB (30.61%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.py
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -10,11 +10,20 @@ class Solution:
             'M' : 1000,
         })
         
-        out = loman[s[0]]
-        for i in range(1, len(s)):
-            if loman[s[i]] <= loman[s[i-1]]:
-                out += loman[s[i]]
-            else:
-                out -= loman[s[i-1]]
-                out += (loman[s[i]] - loman[s[i-1]])
-        return out
+        # out = loman[s[0]]
+        # for i in range(1, len(s)):
+        #     if loman[s[i]] <= loman[s[i-1]]:
+        #         out += loman[s[i]]
+        #     else:
+        #         out -= loman[s[i-1]]
+        #         out += (loman[s[i]] - loman[s[i-1]])
+        # return out
+    
+        ###################
+        number = 0
+        s = s.replace("IV", "IIII").replace("IX", "VIIII")
+        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+        for char in s:
+            number += loman[char]
+        return number
