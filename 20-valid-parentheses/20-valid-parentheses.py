d6e69209944ba89a7facb0d5f0b43fed31d8c3b2
Woom <65762488+WoominM@users.noreply.github.com>
2022-07-14 22:46:15 +0900

    Time: 61 ms (19.68%), Space: 13.9 MB (24.32%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        slist = dict({
+            '(' : ')', 
+            '[' : ']',
+            '{' : '}'
+        })
+        
+        for c in s:
+            if c in list(slist.keys()):
+                stack.append(c)
+            elif stack:
+                last = stack.pop()
+                if slist[last] != c:
+                    return False
+            else:
+                return False
+        return not stack
