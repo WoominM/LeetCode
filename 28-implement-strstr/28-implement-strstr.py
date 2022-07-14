d680cf58bcde05f67d3f8c2f2d11f5c16b5bf519
Woom <65762488+WoominM@users.noreply.github.com>
2022-07-14 23:09:33 +0900

    Time: 53 ms (33.95%), Space: 13.8 MB (63.66%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.py
+++ 28-implement-strstr/28-implement-strstr.py
@@ -4,5 +4,5 @@ class Solution:
         for i in range(lenh-lenn+1):
             if haystack[i:i+lenn] == needle:
                 return i
-        return -1 if haystack != needle else 0
+        return -1
             
