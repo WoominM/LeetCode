61f67b1656ac3f9b09127b2916b70035069c98ab
Woom <65762488+WoominM@users.noreply.github.com>
2022-07-23 03:50:21 +0900

    Time: 218 ms (71.92%), Space: 15.5 MB (65.09%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.py
@@ -0,0 +1,12 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        zeronum = 0
+        for i in range(len(nums)):
+            if nums[i-zeronum] == 0:
+                nums.pop(i-zeronum)
+                zeronum += 1
+        nums += [0] *zeronum
+        return nums 
