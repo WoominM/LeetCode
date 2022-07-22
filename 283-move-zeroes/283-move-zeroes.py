c72cf281b8e2f46a2d243a4e38a7f9c480d670c4
Woom <65762488+WoominM@users.noreply.github.com>
2022-07-23 04:01:50 +0900

    Time: 287 ms (43.36%), Space: 15.5 MB (65.09%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -3,10 +3,17 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        zeronum = 0
-        for i in range(len(nums)):
-            if nums[i-zeronum] == 0:
-                nums.pop(i-zeronum)
-                zeronum += 1
-        nums += [0] * zeronum
-        return nums 
+#         zeronum = 0
+#         for i in range(len(nums)):
+#             if nums[i-zeronum] == 0:
+#                 nums.pop(i-zeronum)
+#                 zeronum += 1
+#         nums += [0] * zeronum
+#         return nums 
+    
+        slow = 0
+        for fast in range(len(nums)):
+            if nums[fast] != 0 and nums[slow] == 0:
+                nums[slow], nums[fast] = nums[fast], nums[slow]
+            if nums[slow] != 0:
+                slow += 1
