1c8778a5d8706eff9b1078871674cfcb57d90623
Woom <65762488+WoominM@users.noreply.github.com>
2022-11-01 06:23:55 +0900

    Time: 309 ms (72.88%), Space: 15.7 MB (17.17%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,6 +1,6 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        slow, fast = 0, 0
+        slow = 0
         for fast in range(len(nums)):
             if nums[fast] != 0:
                 nums[slow], nums[fast] = nums[fast], nums[slow]
