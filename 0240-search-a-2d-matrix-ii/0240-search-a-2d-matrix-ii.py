62f7695009e7c0c700bc7a46997f4730b74228fe
Woom <65762488+WoominM@users.noreply.github.com>
2022-11-02 09:28:07 +0900

    Time: 360 ms (61.51%), Space: 20.5 MB (39.03%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
@@ -1,16 +1,26 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        while matrix:
-            # for nums in matrix:
-            #     print(nums)
-            # print('')
-            m, n = len(matrix), len(matrix[0])
-            if m == 0 or n == 0:
-                return matrix[0] == target
-            if matrix[m-1][0] == target:
-                return True
-            elif matrix[m-1][0] > target:
-                matrix = matrix[:-1]
+        # while matrix:
+        #     # for nums in matrix:
+        #     #     print(nums)
+        #     # print('')
+        #     m, n = len(matrix), len(matrix[0])
+        #     if m == 0 or n == 0:
+        #         return matrix[0] == target
+        #     if matrix[m-1][0] == target:
+        #         return True
+        #     elif matrix[m-1][0] > target:
+        #         matrix = matrix[:-1]
+        #     else:
+        #         matrix = [nums[1:] for nums in matrix]
+        # return False
+    
+        x, y = len(matrix[0]) - 1, 0
+        while x >= 0 and y < len(matrix):
+            if matrix[y][x] > target:
+                x -= 1
+            elif matrix[y][x] < target:
+                y += 1
             else:
-                matrix = [nums[1:] for nums in matrix]
+                return True
         return False
