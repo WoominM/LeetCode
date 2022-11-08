139f7e1d939532f8c14b78fc56497e33d8bc827a
Woom <65762488+WoominM@users.noreply.github.com>
2022-11-08 09:42:46 +0900

    Time: 75 ms (11.54%), Space: 13.8 MB (98.82%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -7,7 +7,7 @@ class Solution:
 
         for i in range(2, len(s) + 1): 
             if int(s[i-1]) != 0:    
-                dp[i] += dp[i - 1]
+                dp[i] = dp[i - 1]
             if 10 <= int(s[i-2:i]) <= 26: 
                 dp[i] += dp[i - 2]
         return dp[len(s)]
