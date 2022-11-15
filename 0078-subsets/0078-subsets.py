27396ac0c034dc9052a9e26c74ef60c495ae90a0
Woom <65762488+WoominM@users.noreply.github.com>
2022-11-16 05:39:39 +0900

    Time: 63 ms (56.96%), Space: 13.9 MB (96.75%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,20 +1,20 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        # subset = [[]]
-        # for num in nums:
-        #     subset += [subnum + [num] for subnum in subset]
-        # return subset
-    
-        subset = [[], nums]
-        def helper(nums):
-            for i in range(len(nums)):
-                subnums = nums[:i] + nums[i+1:]
-                if subnums and subnums not in subset:
-                    subset.append(subnums)
-                    helper(subnums)
-        
-        helper(nums)
+        subset = [[]]
+        for num in nums:
+            subset += [subnum + [num] for subnum in subset]
         return subset
+    
+#         subset = [[], nums]
+#         def helper(nums):
+#             for i in range(len(nums)):
+#                 subnums = nums[:i] + nums[i+1:]
+#                 if subnums and subnums not in subset:
+#                     subset.append(subnums)
+#                     helper(subnums)
+        
+#         helper(nums)
+#         return subset
     
 #     class Solution(object):
 #     def subsets(self, nums):
