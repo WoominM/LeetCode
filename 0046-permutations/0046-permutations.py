aaa0a1966c88f34b95650053d397488d16b46b43
Woom <65762488+WoominM@users.noreply.github.com>
2022-11-16 08:01:43 +0900

    Time: 94 ms (14.13%), Space: 14.1 MB (60.22%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,14 +1,27 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        out = []
-        def helper(i, temp):
-            if i == len(nums):
-                out.append(temp)
-                return
+#         out = []
+#         def helper(i, temp):
+#             if i == len(nums):
+#                 out.append(temp)
+#                 return
             
-            subnums = [num for num in nums if num not in temp]
-            for num in subnums:
-                helper(i + 1, temp + [num])
+#             subnums = [num for num in nums if num not in temp]
+#             for num in subnums:
+#                 helper(i + 1, temp + [num])
                 
-        helper(0, [])
+#         helper(0, [])
+#         return out
+
+
+        def dfs(nums, path, out):
+            if not nums:
+                out.append(path)
+                
+            for i in range(len(nums)):
+                dfs(nums[:i]+nums[i+1:], path+[nums[i]], out)
+        
+        out = []
+        dfs(nums, [], out)
         return out
+    
