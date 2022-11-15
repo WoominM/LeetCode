ec830804431f26dfeb9b34b1172c931716acf31e
Woom <65762488+WoominM@users.noreply.github.com>
2022-11-16 06:20:32 +0900

    Time: 67 ms (15.19%), Space: 13.9 MB (80.34%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,23 +1,27 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:     
-        if len(digits) == 0:
-            return []
-        
         dic = {"2": "abc", "3": "def", "4":"ghi", 
                "5": "jkl", "6": "mno", "7": "pqrs", 
                "8": "tuv", "9": "wxyz"}
         
-        out = []
-        def helper(i, subc):
-            if i == len(digits):
-                out.append(subc)
-                return
-            
-            for digit in dic[digits[i]]:
-                helper(i + 1, subc + digit)
+        if len(digits) == 0:
+            return []
         
-        helper(0, '')
-        return out
+#         out = []
+#         def helper(i, subc):
+#             if i == len(digits):
+#                 out.append(subc)
+#                 return
             
+#             for digit in dic[digits[i]]:
+#                 helper(i + 1, subc + digit)
+        
+#         helper(0, '')
+#         return out
+    
+        out = ['']
+        for digit in digits:
+            out = [item + c for item in out for c in dic[digit]]
+        return out
     
     
