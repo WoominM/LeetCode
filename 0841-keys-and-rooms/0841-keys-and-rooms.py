97708bd396a5a39e672276270b10499103281aa6
Woom <65762488+WoominM@users.noreply.github.com>
2022-11-17 05:45:09 +0900

    Time: 163 ms (18.81%), Space: 14.4 MB (56.18%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.py
+++ 0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -1,12 +1,27 @@
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
 
-        def helper(room):
-            if room not in visited: 
-                visited.add(room)
-                for key in rooms[room]:
-                    helper(key)
+#         def helper(room):
+#             # print(room, visited)
+#             if room not in visited: 
+#                 visited.add(room)
+#                 for key in rooms[room]:
+#                     helper(key)
                     
+#         visited = set()
+#         helper(0)
+#         return len(visited) == len(rooms)
+        
+        from collections import deque
         visited = set()
-        helper(0)
+        visited.add(0)
+        que = deque()
+        que.append(0)
+        while que:
+            room = que.popleft()
+            print(room)
+            for key in rooms[room]:
+                if key not in visited:
+                    que.append(key)
+                    visited.add(key)
         return len(visited) == len(rooms)
