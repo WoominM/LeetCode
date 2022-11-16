e41077b22964f0488b0062b9ead7e3c19e159221
Woom <65762488+WoominM@users.noreply.github.com>
2022-11-17 08:20:15 +0900

    Time: 377 ms (63.44%), Space: 14.6 MB (28.93%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.py
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -1,19 +1,39 @@
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
-        from collections import deque
+        # from collections import deque
+        # colored = [0] * len(graph)
+        # for i in range(len(graph)):
+        #     if colored[i] == 0:
+        #         colored[i] = -1
+        #         que = deque([i])
+        #         while que:
+        #             idx = que.popleft()
+        #             for node in graph[idx]:
+        #                 if colored[node] == colored[idx]:
+        #                     return False
+        #                 if colored[node] == 0:
+        #                     colored[node] = -1 * colored[idx]
+        #                     que.append(node)            
+        # return True
+    
+        def helper(i, c):
+            if not self.out:
+                return
+            if colored[i] == 0:
+                colored[i] = -c
+                
+            for node in graph[i]:
+                if colored[node] == colored[i]:
+                    self.out = False
+                    return
+                if colored[node] == 0:
+                    helper(node, colored[i])
         
+        self.out = True
         colored = [0] * len(graph)
         for i in range(len(graph)):
             if colored[i] == 0:
-                colored[i] = -1
-                que = deque([i])
-                while que:
-                    idx = que.popleft()
-                    for node in graph[idx]:
-                        if colored[node] == colored[idx]:
-                            return False
-                        if colored[node] == 0:
-                            colored[node] = -1 * colored[idx]
-                            que.append(node)
-        # print(colored)            
-        return True
+                helper(i, 1)
+        # print(colored)
+        return self.out
+        
