7bb2b4346833610f33b6b27fce297f1cdc140dc8
Woom <65762488+WoominM@users.noreply.github.com>
2022-11-16 13:39:58 +0900

    Time: 68 ms (53.51%), Space: 13.9 MB (84.23%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -0,0 +1,20 @@
+class Solution:
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        
+        def helper(nums, path):
+            if len(path) > 8:
+                return
+            if not nums:
+                if len(path) == 8:
+                    out.append(''.join(path[:-1]))
+                return
+            
+            helper(nums[1:], path + [nums[:1], '.'])
+            if 10 <= int(nums[:2]):
+                helper(nums[2:], path + [nums[:2], '.'])
+            if 100 <= int(nums[:3]) <= 255:
+                helper(nums[3:], path + [nums[:3], '.'])
+        
+        out = []
+        helper(s, [])
+        return out
