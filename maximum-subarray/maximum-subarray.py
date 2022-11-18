fb8daa239a9438df2bda63999b45b07ee6130c12
anonymousmwm <auddnals@gmail.com>
2022-11-18 18:10:47 +0900

    update

--- maximum-subarray/maximum-subarray.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:        
-        sub = [nums[0]]
-        for i,num in enumerate(nums[1:], start=1):
-            sub.append(max(sub[i-1] + num, num))
-        return max(sub)
