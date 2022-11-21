a9bb776b7e3b1e5784ddc07397b7a3ad9e101b6e
Woom <65762488+WoominM@users.noreply.github.com>
2022-11-21 17:06:01 +0900

    Time: 5866 ms (6.46%), Space: 14.8 MB (32.34%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,14 +1,36 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        array = [-1] * (amount + 1)
-        array[0] = 0
-        for n in range(1, amount+1):
-            for i in coins:
-                if n-i >= 0 and array[n-i] != -1:
-                    if array[n] == -1:
-                        array[n] = float('inf')
-                    array[n] = min(array[n-i], array[n])
-                # print(n, i, n-i, array)
-            if array[n] != -1:
-                array[n] += 1
-        return array[-1] if array[-1] != -1 else -1
+        if amount == 0:
+            return 0
+        
+#         def helper(path, value):
+#             print(value, path)
+#             if value == 0:
+#                 if not self.out:
+#                     self.out = path
+#                 elif len(path) < len(self.out):
+#                     self.out = path
+#             if value < 0:
+#                 return
+#             for coin in coins:
+#                 helper(path + [coin], value - coin)
+        
+#         self.out = []
+#         helper([], amount)
+#         return len(self.out) if self.out else -1
+
+        dp = [-1] * (amount + 1)
+        for i in range(1, amount + 1):
+            for coin in coins:
+                if i < coin:
+                    continue
+                elif i == coin:
+                    dp[i] = 1
+                elif dp[i-coin] == -1:
+                    continue
+                elif dp[i] == -1:
+                    dp[i] = dp[i-coin] + 1
+                else:
+                    dp[i] = min(dp[i], dp[i-coin] + 1)
+        print(dp)
+        return dp[-1]
