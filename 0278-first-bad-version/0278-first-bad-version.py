6a0cea3424d5f8a4b0f027d86eaf331ba5058c0b
Woom <65762488+WoominM@users.noreply.github.com>
2022-11-22 16:37:21 +0900

    Time: 57 ms (40.24%), Space: 13.8 MB (97.58%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.py
@@ -0,0 +1,15 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        left, right = 0, n
+        
+        while left < right:
+            pivot = (left + right) // 2
+            if isBadVersion(pivot):
+                right = pivot
+            else:
+                left = pivot + 1
+                
+        return left
