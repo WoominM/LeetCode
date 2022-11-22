be4f40d11ab59819d192d0cc9e5766e3d08e2398
Woom <65762488+WoominM@users.noreply.github.com>
2022-11-22 15:08:59 +0900

    Time: 179 ms (34.12%), Space: 14.3 MB (15.77%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,21 +1,23 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         
-        def helper(i, j, c):
-            if i < 0 or i == m or j < 0 or j == n:
+        def helper(i, j):
+            if i < 0 or i >= m or j < 0 or j >= n:
                 return
-            if image[i][j] != c:
+            if (i, j) in visited:
                 return
-            if visited[i][j] == 1:
+            if image[i][j] != prev_c:
                 return
-            visited[i][j] = 1
             image[i][j] = color
+            visited.add((i, j))
+            
             for x, y in offset:
-                helper(i + x, j + y, c)
-                
-        offset = [[-1, 0], [1, 0], [0, -1], [0, 1]]
-        m, n = len(image), len(image[0])
-        visited = [[0] * n for _ in range(m)]
-        helper(sr, sc, image[sr][sc])
-        return image
+                helper(i + x, j + y)
         
+        visited = set()
+        offset = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+        
+        m, n = len(image), len(image[0])
+        prev_c = image[sr][sc]
+        helper(sr, sc)
+        return image
