9a7f80ca1571f9a2c8b20e62f664c3170162ea7f
Woom <65762488+WoominM@users.noreply.github.com>
2022-11-23 13:42:27 +0900

    Time: 2319 ms (9.68%), Space: 72.4 MB (15.36%) - LeetHub

---
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.py
@@ -0,0 +1,34 @@
+class TimeMap:
+
+    def __init__(self):
+        self.hdic = collections.defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.hdic[key] += [[value, timestamp]]
+        
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.hdic:
+            return ''
+        
+        nums = self.hdic[key]
+        target = timestamp
+        
+        left, right = 0, len(nums)
+        while left < right:
+            pivot = (left + right) // 2
+            if nums[pivot][1] == target:
+                return nums[pivot][0]
+            if nums[pivot][1] < target:
+                left = pivot + 1
+            elif nums[pivot][1] > target:
+                right = pivot
+        return '' if right == 0 else nums[right-1][0]
+
+        arr = self.dic[key]
+        n = len(arr)
+
+    
+# Your TimeMap object will be instantiated and called as such:
+# obj = TimeMap()
+# obj.set(key,value,timestamp)
+# param_2 = obj.get(key,timestamp)
