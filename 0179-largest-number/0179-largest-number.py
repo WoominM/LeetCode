a50ddbf237d57ae776a89bf39a33b93bbe631bdd
Woom <65762488+WoominM@users.noreply.github.com>
2022-11-24 17:09:21 +0900

    Time: 61 ms (77.88%), Space: 13.9 MB (68.40%) - LeetHub

--- 0179-largest-number/0179-largest-number.py
+++ 0179-largest-number/0179-largest-number.py
@@ -1,5 +1,23 @@
 class Solution:
-    # def largestNumber(self, nums: List[int]) -> str:
+    def largestNumber(self, nums: List[int]) -> str:
+
+        def cmp_func(x, y):
+            """Sorted by value of concatenated string increasingly."""
+            if x + y > y + x:
+                return 1
+            elif x == y:
+                return 0
+            else:
+                return -1
+            
+        # Build nums contains all numbers in the String format.
+        nums = [str(num) for num in nums]
+        
+        # Sort nums by cmp_func decreasingly.
+        nums.sort(key = cmp_to_key(cmp_func), reverse = True)
+        
+        # Remove leading 0s, if empty return '0'.
+        return ''.join(nums).lstrip('0') or '0'
     
     def compare(self, n1, n2):
         return str(n1) + str(n2) > str(n2) + str(n1)
@@ -32,23 +50,23 @@ class Solution:
 #         return res
 
     # quick sort, in-place
-    def largestNumber(self, nums):
-        self.quickSort(nums, 0, len(nums)-1)
-        return str(int("".join(map(str, nums)))) 
+#     def largestNumber(self, nums):
+#         self.quickSort(nums, 0, len(nums)-1)
+#         return str(int("".join(map(str, nums)))) 
 
-    def quickSort(self, nums, l, r):
-        if l >= r:
-            return 
-        pos = self.partition(nums, l, r)
-        self.quickSort(nums, l, pos-1)
-        self.quickSort(nums, pos+1, r)
+#     def quickSort(self, nums, l, r):
+#         if l >= r:
+#             return 
+#         pos = self.partition(nums, l, r)
+#         self.quickSort(nums, l, pos-1)
+#         self.quickSort(nums, pos+1, r)
 
-    def partition(self, nums, l, r):
-        low = l
-        while l < r:
-            if self.compare(nums[l], nums[r]):
-                nums[l], nums[low] = nums[low], nums[l]
-                low += 1
-            l += 1
-        nums[low], nums[r] = nums[r], nums[low]
-        return low
+#     def partition(self, nums, l, r):
+#         low = l
+#         while l < r:
+#             if self.compare(nums[l], nums[r]):
+#                 nums[l], nums[low] = nums[low], nums[l]
+#                 low += 1
+#             l += 1
+#         nums[low], nums[r] = nums[r], nums[low]
+#         return low
