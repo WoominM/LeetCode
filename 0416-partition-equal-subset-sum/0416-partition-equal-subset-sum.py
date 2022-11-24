6b0ce3ea1cb600419e9702cdbe05e8d4b76794bd
Woom <65762488+WoominM@users.noreply.github.com>
2022-11-24 15:08:17 +0900

    Time: 5072 ms (23.09%), Space: 645.7 MB (5.01%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,23 +1,24 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-#         @cache
-#         def helper(i, value):
-#             if i == n:
-#                 return value * 2 == target
-#             if helper(i + 1, value + nums[i]):
-#                 return True
-#             if helper(i + 1, value):
-#                 return True
-            
-#         n = len(nums)
-#         target = sum(nums)
-#         return False if target == 1 else helper(0, 0)
-    
         @cache
-        def subsetSum(s, i):
-            if s == 0: return True
-            if i >= len(nums) or s < 0: return False
-            return subsetSum(s-nums[i], i+1) or subsetSum(s, i+1)
-        total_sum = sum(nums)
-        return total_sum & 1 == 0 and subsetSum(total_sum // 2, 0)
+        def helper(i, value):
+            if i == n:
+                return value * 2 == target
+            if value * 2 > target:
+                return False
+            return helper(i + 1, value + nums[i]) or helper(i + 1, value)
+            
+        n = len(nums)
+        target = sum(nums)
+        return False if target == 1 else helper(0, 0)
+    
+        # @cache
+        # def subsetSum(s, i):
+        #     if s == 0: 
+        #         return True
+        #     if i >= len(nums) or s < 0: 
+        #         return False
+        #     return subsetSum(s-nums[i], i+1) or subsetSum(s, i+1)
+        # total_sum = sum(nums)
+        # return total_sum & 1 == 0 and subsetSum(total_sum // 2, 0)
         
